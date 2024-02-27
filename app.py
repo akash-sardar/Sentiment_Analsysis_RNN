7cc21b895124b27f1f3eb7f3014aaa295fccd1c8
Akash-Sardar <lordakashjoy@gmail.com>
2024-02-27 00:43:18 -0500

    removed autograd.variable

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.autograd import Variable
+#from torch.autograd import Variable
 #--------------------------------------------------------------#
 
 app = Flask(__name__)
